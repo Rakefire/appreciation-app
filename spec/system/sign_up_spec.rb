f994843fb0dce61bfdfb4a44f12107451af65073
Ricky Chilcott <ricky@rakefire.io>
2018-08-09 14:58:52 -0400

    Add spec for sign up

---
+++ spec/system/sign_up_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "sign up ", type: :system do
+
+  it 'allows user to sign up' do
+    visit root_path
+
+    perform_enqueued_jobs do
+      click_on "Sign Up!"
+      fill_in "Email", with: "ricky@rickychilcott.com"
+      click_on "Create Sign up"
+
+      mail = email_with_subject("Verifying Email Address")
+      expect(mail.to).to include("ricky@rickychilcott.com")
+    end
+  end
+end
